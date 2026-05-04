e64f028fd4182e18895c59168b68bf4fad008c44
daniel <daniel.chikmamat@gmail.com>
2026-05-05 00:50:46 +0200

    feat: dependencies to get kafka connection

--- app/api/orders.py
+++ app/api/orders.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter, Depends
 from app.schemas.order import CreateOrderRequest
 from app.services.order_service import OrderService
+from app.deps.order import get_order_service
 
 router = APIRouter(prefix="/orders")
 
 @router.post("")
 async def create_order(
     request: CreateOrderRequest,
-    service: OrderService = Depends()
+    service: OrderService = Depends(get_order_service)
 ):
     return await service.create_order(request)
