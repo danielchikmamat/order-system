b4a6b0d0947399391466a17d79e97a2b1c93824d
daniel <daniel.chikmamat@gmail.com>
2026-05-09 14:11:40 +0200

    feat: database url in settings

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     KAFKA_BOOTSTRAP: str
     KAFKA_TOPIC: str
+    DATABASE_URL: str
 
 
     model_config = SettingsConfigDict(env_file=".env")
