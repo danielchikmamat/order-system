c8b4213045daa78177d47b774cc192a0d8a83187
daniel <daniel.chikmamat@gmail.com>
2026-05-09 15:42:50 +0200

    feat: model updated

--- app/domain/models.py
+++ app/domain/models.py
@@ -1,8 +1,9 @@
 import uuid
 from datetime import datetime, timezone
-from sqlalchemy import Column, String, Float, DateTime, ForeignKey
+from sqlalchemy import Column, String, Float, Integer,DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from app.infrastructure.db.engine import Base
+
 
 class Order(Base):
     __tablename__ = "orders"
@@ -15,3 +16,15 @@ class Order(Base):
     total_amount = Column(Float, default=0.0)
 
     items = relationship("OrderItem", back_populates="order", cascade="all, delete-orphan")
+
+
+class OrderItem(Base):
+    __tablename__ = "order_items"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    order_id = Column(String(36), ForeignKey("orders.id"), nullable=False)
+    product_id = Column(String, nullable=False, index=True)
+    quantity = Column(Integer, nullable=False)
+    unit_price = Column(Float, default=0.0)
+
+    order = relationship("Order", back_populates="items")
